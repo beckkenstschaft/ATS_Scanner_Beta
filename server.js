e08aec81706d4827c17205676e843c86aea0da8c
amaan3091 <amaan.work96@gmail.com>
2024-09-09 12:59:52 +0530

    Removed unused libraries

--- server.js
+++ server.js
@@ -1,12 +1,5 @@
 const express = require("express");
-// const multer = require("multer");
-// const pdfParse = require("pdf-parse");
-// const docxParser = require("docx-parser");
 const path = require("path");
-// const fs = require("fs");
-
-// const spellChecker = require("spellchecker");
-// const GrammarBot = require("grammarbot");
 
 const app = express();
 
